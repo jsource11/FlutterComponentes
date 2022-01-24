828797b293694de1e0311361af2fa8be8b9ac4f7
JJGU <jjgu.tec@gmail.com>
2022-01-23 22:23:22 -0500

    read file json

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
+import 'package:componentes_app/providers/menu_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,12 +11,44 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Componentes'),
       ),
-      // body: ListView(children: _crearListaItems()),
-      body: ListView(children: _crearListaItemsCorta()),
+      //body: ListView(children: _crearListaItems()),
+      //body: ListView(children: _lista()),
+      body: _lista(),
     );
   }
 
-  List<Widget> _crearListaItems() {
+  Widget _lista() {
+    return FutureBuilder(
+        future: menuProvider.cargarData(),
+        initialData: [],
+        builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+          return ListView(children: _listaItems(snapshot.data ?? []));
+        });
+  }
+
+  List<Widget> _listaItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+    data.forEach((element) {
+      final wt = ListTile(
+          title: Text(element['texto']),
+          leading: const Icon(
+            Icons.account_circle,
+            color: Colors.blue,
+          ),
+          trailing: const Icon(
+            Icons.keyboard_arrow_right,
+            color: Colors.blue,
+          ),
+          onTap: () {});
+
+      opciones.add(wt);
+      opciones.add(Divider());
+    });
+
+    return opciones;
+  }
+
+  /*List<Widget> _crearListaItems() {
     List<String> items = [
       'Enero',
       'Febrero',
@@ -39,11 +73,11 @@ class HomePage extends StatelessWidget {
       lista.add(tempItem);
       lista.add(Divider());
     }
-
     return lista;
-  }
+  }*/
 
-  List<Widget> _crearListaItemsCorta() {
+  /*List<Widget> _crearListaItemsCorta() {
+    //menuProvider.cargarData()
     List<String> items = [
       'Enero',
       'Febrero',
@@ -73,5 +107,5 @@ class HomePage extends StatelessWidget {
         ],
       );
     }).toList();
-  }
+  }*/
 }
