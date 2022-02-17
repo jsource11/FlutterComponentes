6efbbc6c0fc3a4e636f9ed138c897af192a160b8
JJGU <jjgu.tec@gmail.com>
2022-02-17 01:03:46 -0500

    add card

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -9,6 +9,35 @@ class CardPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Cards'),
       ),
+      body: ListView(
+        padding: const EdgeInsets.all(20.0),
+        children: [_cardTipo1()],
+      ),
+    );
+  }
+
+  Widget _cardTipo1() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(
+              Icons.photo_album,
+              color: Colors.blue,
+            ),
+            title: Text("Titulo del Card"),
+            subtitle: Text(
+                "Descripción de las tarjetas para mostrar los componentes de Flutter, Descripción de las tarjetas para mostrar los componentes de Flutter"),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FlatButton(onPressed: () {}, child: Text("Cancelar")),
+              FlatButton(onPressed: () {}, child: Text("Ok"))
+            ],
+          )
+        ],
+      ),
     );
   }
 }
