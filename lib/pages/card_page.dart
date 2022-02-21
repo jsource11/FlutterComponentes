67dab2fc6030ca4ac4952fa859b51d8a2a7f9f31
JJGU <jjgu.tec@gmail.com>
2022-02-20 21:50:18 -0500

    cards and images

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -11,16 +11,43 @@ class CardPage extends StatelessWidget {
       ),
       body: ListView(
         padding: const EdgeInsets.all(20.0),
-        children: [_cardTipo1()],
+        children: [
+          _cardTipo1(),
+          const SizedBox(
+            height: 20.0,
+          ),
+          _cardTipo2(),
+          const SizedBox(
+            height: 20.0,
+          ),
+          _cardTipo2(),
+          const SizedBox(
+            height: 20.0,
+          ),
+          _cardTipo2(),
+          const SizedBox(
+            height: 20.0,
+          ),
+          _cardTipo2(),
+          const SizedBox(
+            height: 20.0,
+          ),
+          _cardTipo2(),
+          const SizedBox(
+            height: 20.0,
+          ),
+        ],
       ),
     );
   }
 
   Widget _cardTipo1() {
     return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
       child: Column(
         children: <Widget>[
-          ListTile(
+          const ListTile(
             leading: Icon(
               Icons.photo_album,
               color: Colors.blue,
@@ -37,6 +64,50 @@ class CardPage extends StatelessWidget {
             ],
           )
         ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    final card = Card(
+      clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: [
+          const FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+            image: NetworkImage(
+                'https://s.libertaddigital.com/fotos/noticias/1920/1080/fit/tunez-la-guerra-de-las-galaxias.jpg'),
+            height: 200.0,
+            fit: BoxFit.cover,
+          ),
+
+          /*const Image(
+            image: NetworkImage(
+                'https://s.libertaddigital.com/fotos/noticias/1920/1080/fit/tunez-la-guerra-de-las-galaxias.jpg'),
+          ),*/
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text("Tatooine"),
+          )
+        ],
+      ),
+    );
+
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30.0),
+          color: Colors.white,
+          boxShadow: const [
+            BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10.0,
+                spreadRadius: 2.0,
+                offset: Offset(2.0, 10.0))
+          ]),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30.0),
+        child: card,
       ),
     );
   }
